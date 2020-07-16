258300993c58ad542fe593df7d4c732bbc934e9e
mghelton <gagehelton@gmail.com>
2020-07-16 15:13:48 -0400

    put subtree in list and pop 0 index

--- bulk.py
+++ bulk.py
@@ -1,5 +1,6 @@
 from pysnmp.hlapi import *
 
+l = []
 for errorIndication, errorStatus, \
     errorIndex, varBinds in bulkCmd(
         SnmpEngine(),
@@ -7,7 +8,7 @@ for errorIndication, errorStatus, \
         UdpTransportTarget(('192.168.61.6', 161)),
         ContextData(),
         0, 50,  # GETBULK specific: request up to 50 OIDs in a single response
-        ObjectType(ObjectIdentity('IF-MIB', 'ifIndex').addAsn1MibSource('file:///Users/michaelhelton/Downloads/mibBrowser/IF-MIB')),
+        ObjectType(ObjectIdentity('IF-MIB', 'ifDescr').addAsn1MibSource('file:///Users/michaelhelton/Downloads/mibBrowser/IF-MIB')),
         lookupMib=False, lexicographicMode=False):
 
     if errorIndication:
@@ -19,5 +20,8 @@ for errorIndication, errorStatus, \
         break
     else:
         for varBind in varBinds:
-            
-            print(' = '.join([x.prettyPrint() for x in varBind]))
+            l.append(' = '.join([x.prettyPrint() for x in varBind]))
+    
+l.pop(0)
+for i in l:
+    print(i)
